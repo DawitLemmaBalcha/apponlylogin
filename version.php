d8b143251dbc2e552b5c4b530bee3c844a031457
Dawit_Lemma_Balcha <dawit.lemma.balcha@gmail.com>
2025-09-13 14:00:11 +0300

    Create version.php

---
+++ version.php
@@ -0,0 +1,6 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+$plugin->component = 'local_apponlylogin';
+$plugin->version   = 2025090704; // New version number
+$plugin->requires  = 2024051300;
+$plugin->maturity  = MATURITY_STABLE;
