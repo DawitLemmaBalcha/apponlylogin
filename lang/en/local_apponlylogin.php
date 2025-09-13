cd8d5d02aa9b8dc389fa4d330ce47b80db590a32
Dawit_Lemma_Balcha <dawit.lemma.balcha@gmail.com>
2025-09-13 14:07:40 +0300

    Create local_apponlylogin.php

---
+++ lang/en/local_apponlylogin.php
@@ -0,0 +1,5 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'App Only Login';
+$string['student_login_blocked'] = 'Web login is not available for your role. Please use the mobile app to access the site.';
